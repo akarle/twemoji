b478ae0a03bc367520d3e6843e657f209a68b4dd
Makenzie Schwartz <schwartz.makenzie@gmail.com>
2017-10-24 14:34:26 -0400

    added data and num_instances flags to run_me

--- Code/run_me.py
+++ Code/run_me.py
@@ -14,6 +14,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-c', '--classifier', nargs='+', default='all', choices=['nb', 'lr'],
     help='specifies which classifier(s) to use (default: %(default)s) possible classifiers: %(choices)s',
     metavar='C', dest='classifier_type')
+parser.add_argument('-d', '--data', nargs=1, default='train',
+    help='name of dataset file to use (must be in Data directory)',
+    metavar='dataset', dest='data')
+parser.add_argument('-n', nargs=1,
+    help='number of data entries to train/evaluate on',
+    metavar='N', dest='num_instances')
 args = parser.parse_args()
 print(args)
 
