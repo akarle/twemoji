6ef48ae520c67d9ee887fb55b411061acb29e2c6
Makenzie Schwartz <schwartz.makenzie@gmail.com>
2017-10-23 22:31:42 -0400

    began adding arg parsing to run_me.py

--- Code/run_me.py
+++ Code/run_me.py
@@ -1,4 +1,14 @@
 # Imports
+import argparse
+
+# Parse arguments
+parser = argparse.ArgumentParser(
+    description="Run emoji prediction classifiers and output accuracy results.")
+parser.add_argument('-c', '--classifier', nargs='+', default='all', choices=['nb', 'lr'],
+    help='specifies which classifier(s) to use (default: %(default)s) possible classifiers: %(choices)s',
+    metavar='C', dest='classifier_type')
+args = parser.parse_args()
+print(args)
 
 # Load Data
 
