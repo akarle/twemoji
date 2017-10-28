623eb46fae7cda38d1141c026eefb2b4707b42ec
Alexander Karle <akarle@umass.edu>
2017-10-27 20:58:46 -0400

    added load_data.py and an example of count vectorizer

--- Code/sent_analysis.py
+++ Code/sent_analysis.py
@@ -1,6 +1,7 @@
 # Imports
 from sklearn.naive_bayes import GaussianNB
 
+
 def baseline_sentiment(data):
-	""" Baseline Sentiment of Ngrams and NB """
-	pass
+    """ Baseline Sentiment of Ngrams and NB """
+    
