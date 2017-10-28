339fd47f9dd734010b2e92a528ffdb8c8af9710c
Alexander Karle <akarle@umass.edu>
2017-10-28 14:27:38 -0400

    tabs to spaces

--- Code/feature_extractor.py
+++ Code/feature_extractor.py
@@ -1,20 +1,20 @@
 # Imports
 
 class FeatureExtractor():
-	""" A class to extract text features """
-	def __init__(self, arg):		
-		self.arg = arg
+    """ A class to extract text features """
+    def __init__(self, arg):
+        self.arg = arg
 
-	def unary_features(self, data):
-		""" Extract Unary Features """
-		pass
+    def unary_features(self, data):
+        """ Extract Unary Features """
+        pass
 
-	def sent_analysis(self, data):
-		""" Extracts Sentiment Analysis Features via sent_analysis.py """
-		pass
+    def sent_analysis(self, data):
+        """ Extracts Sentiment Analysis Features via sent_analysis.py """
+        pass
 
-	def extract_features(self, data, feat_types):
-		""" External function to call to extract multiple features"""
+    def extract_features(self, data, feat_types):
+        """ External function to call to extract multiple features"""
 
-		#thought: you'd pass [unary, binary] and extract and return both
+        #thought: you'd pass [unary, binary] and extract and return both
 
--- Code/plotting.py
+++ Code/plotting.py
@@ -2,5 +2,5 @@
 from matplotlib import pyplot as plt
 
 def acc_bar_chart(baseline, acc_scores, labels):
-	""" Graph the given accuracies against each other and baseline """
-	pass
+    """ Graph the given accuracies against each other and baseline """
+    pass
