e20a223e875f49360043cecb155810dcac10bb29
Makenzie Schwartz <mawaschwa@gmail.com>
2017-11-16 17:32:19 -0500

    added load/scoring functions for emolex data

--- Code/load_data.py
+++ Code/load_data.py
@@ -5,6 +5,7 @@
 """
 import csv
 import os
+from collections import defaultdict
 
 
 def load_data(data_file_path, label_file_path, num_instances=float('inf')):
@@ -83,23 +84,28 @@ def load_sent140(data_path, word_limit=float('inf')):
     return (trdata, trlabels, tedata, telabels)
 
 
-def load_nrc_emotion_lexicon(data_path):
+def load_nrc_emotion_lexicon(
+    data_path='../Data/Lexicons/' +
+        'NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt'):
     emolex = {}
-    emolex['anger'] = {}
-    emolex['anticipation'] = {}
-    emolex['disgust'] = {}
-    emolex['fear'] = {}
-    emolex['joy'] = {}
-    emolex['positive'] = {}
-    emolex['negative'] = {}
-    emolex['sadness'] = {}
-    emolex['surprise'] = {}
-    emolex['trust'] = {}
+    emolex['anger'] = defaultdict(int)
+    emolex['anticipation'] = defaultdict(int)
+    emolex['disgust'] = defaultdict(int)
+    emolex['fear'] = defaultdict(int)
+    emolex['joy'] = defaultdict(int)
+    emolex['positive'] = defaultdict(int)
+    emolex['negative'] = defaultdict(int)
+    emolex['sadness'] = defaultdict(int)
+    emolex['surprise'] = defaultdict(int)
+    emolex['trust'] = defaultdict(int)
     with open(data_path, 'rb') as fp:
         reader = csv.reader(fp, delimiter='\t')
+        data = []
         for row in reader:
-            print row
-            #emolex[row[1]][row[0]] = int(row[2])
+            data.append(row)
+        for d in data:
+            if len(d) == 3:
+                emolex[str(d[1])][str(d[0])] = int(d[2])
     # for d in emolex:
-    #     print d['phony']
+    #     print emolex[d]['phony']
     return emolex
