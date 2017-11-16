f5a75a7685dfcca4dc84d3a657c4b71768c38f61
Makenzie Schwartz <mawaschwa@gmail.com>
2017-11-16 17:32:19 -0500

    added load/scoring functions for emolex data

--- Code/sentiment_lexicon_scoring.py
+++ Code/sentiment_lexicon_scoring.py
@@ -10,11 +10,12 @@ from nltk.corpus import sentiwordnet as swn
 from nltk.stem import WordNetLemmatizer
 from CMUTweetTagger import runtagger_parse
 import os
-from load_data import load_sent140
+from load_data import load_nrc_emotion_lexicon as load_emolex
 import csv
 import unicodedata
 import sys
 import random
+import re
 
 
 def sentiwordnet_score(text):
@@ -30,7 +31,7 @@ def sentiwordnet_score(text):
     # print ws
     acceptable_pos = ['N', 'O', 'S', 'Z', 'V', 'A', 'R']
     wnl = WordNetLemmatizer()
-    ws = [wnl.lemmatize(w[0].lower(), ark_to_swn(w[1])) +
+    ws = [wnl.lemmatize(w[0].lower()) +
           '.' + ark_to_swn(w[1]) + '.01'
           for w in ws if w[1] in acceptable_pos]
     # print ws
@@ -62,6 +63,32 @@ def sentiwordnet_classify(text):
         return 0
 
 
+def emolex_score(tlist):
+    """
+    tlist: a list of strings
+    """
+    feats = []
+    emolex = load_emolex()
+    wnl = WordNetLemmatizer()
+    for t in tlist:
+        tfeat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for word in re.findall(r'(?u)\b\w+\b', t.lower()):
+            #print word
+            word = wnl.lemmatize(word)
+            tfeat[0] += emolex['anger'][word]
+            tfeat[1] += emolex['anticipation'][word]
+            tfeat[2] += emolex['disgust'][word]
+            tfeat[3] += emolex['fear'][word]
+            tfeat[4] += emolex['joy'][word]
+            tfeat[5] += emolex['positive'][word]
+            tfeat[6] += emolex['negative'][word]
+            tfeat[7] += emolex['sadness'][word]
+            tfeat[8] += emolex['surprise'][word]
+            tfeat[9] += emolex['trust'][word]
+        feats.append(tfeat)
+    return feats
+
+
 def ark_to_swn(pos):
     if pos in ['N', 'O', '^', 'S', 'Z']:
         return 'n'
@@ -88,33 +115,43 @@ def test_classifier(numinstances):
         ncorrect = 0.0
         pwrong = 0.0
         nwrong = 0.0
+        neutral = 0
         count = 0
         for row in data:
             if row[1] == 'Sentiment':
                 continue
+            gold = int(row[1])
             if count % 10 == 0:
                 sys.stdout.write('.')
                 sys.stdout.flush()
             if count == numinstances:
                 break
             try:
-                predict = sentiwordnet_classify(row[3])
+                sm = emolex_score([row[3]])[0]
+                #print sm
+                pos_pre = sm[5]
+                neg_pre = sm[6]
+                if pos_pre == neg_pre:
+                    if gold == 0:
+                        neutral -= 1
+                    else:
+                        neutral += 1
+                    continue
+                predict = pos_pre >= neg_pre
             except Exception:
                 continue
-            gold = int(row[1])
-            if (predict in [1, 0] and gold is 1 or
-                    predict is -1 and gold is 0):
-                if predict in [1, 0]:
+            if predict:
+                if gold is 1:
                     pcorrect += 1
                 else:
-                    ncorrect += 1
-            else:
-                if predict in [1, 0]:
                     nwrong += 1
-                else:
+            else:
+                if gold is 1:
                     pwrong += 1
+                else:
+                    ncorrect += 1
             count += 1
         acc = (pcorrect + ncorrect) / (pcorrect + ncorrect + pwrong + nwrong)
         pre = pcorrect / (pcorrect + nwrong)
         rec = pcorrect / (pcorrect + pwrong)
-        print '\n',  acc, pre, rec
+        print '\n', acc, pre, rec, neutral
